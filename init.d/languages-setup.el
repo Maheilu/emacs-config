9d23bceb930b3f8a46bc34f6f9d4658e61806288
Maheilu <96106270+Maheilu@users.noreply.github.com>
2023-09-15 12:17:11 +0200

    elixir-ts language server und fish shell

--- init.d/languages-setup.el
+++ init.d/languages-setup.el
@@ -1,9 +1,6 @@
 (require 'dash)
 
-;; tree-sitter part 1
-(if (equal system-type 'gnu/linux)
-    (defconst use-tree-sitter t)
-  (defconst use-tree-sitter nil))
+(defconst use-tree-sitter (equal system-type 'gnu/linux))
 
 ;; generic programming modes
 (mapc (lambda (hook) (add-hook hook #'eglot-ensure))
@@ -35,10 +32,14 @@
   (add-buffer-local-sub-hook 'python-mode-hook 'before-save-hook #'eglot-format-buffer))
 ;; Elixir
 (if use-tree-sitter
-    (elpaca elixir-ts-mode)
+    (progn (elpaca elixir-ts-mode)
+           (with-eval-after-load 'eglot
+             (add-to-list 'eglot-server-programs
+                          `((elixir elixir-ts-mode heex-ts-mode) .
+                            ,(eglot-alternatives '("language_server.sh" "~/Builds/lexical/_build/dev/package/lexical/bin/start_lexical.sh"))))))
   (elpaca elixir-mode))
 
-;; tree-sitter part 2
+;; tree-sitter
 (when use-tree-sitter 
   (setq treesit-language-source-alist
 	    '((cpp "https://github.com/tree-sitter/tree-sitter-cpp")
